86f59daae17f428374b5017d194943c83e6220f3
Ricky Mormor <rickymormor1@gmail.com>
2022-01-27 18:28:45 +0000

    Add and remove books from API

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,40 +1,41 @@
 import { useDispatch } from 'react-redux';
 import { v4 as uuidv4 } from 'uuid';
 import { useState } from 'react';
-import { addBook, removeBook } from '../redux/books/books';
+import { addBookAPI } from '../redux/books/books';
 import BookList from './BookList';
 
 const Books = function ShowBooks() {
   const dispatch = useDispatch();
   const [title, setTitle] = useState('');
-  const [author, setAuthor] = useState('');
+  const [category, setCategory] = useState('');
 
   const submitBookToStore = () => {
-    if (title === '' || author === '') {
+    if (title === '' || category === '') {
       return;
     }
 
     const newBook = {
-      id: uuidv4(),
+      item_id: uuidv4(),
       title,
-      author,
+      category,
     };
 
-    dispatch(addBook(newBook));
+    dispatch(addBookAPI(newBook));
   };
 
-  const removeBookFromStore = (id) => {
-    dispatch(removeBook(id));
-  };
+  // eslint-disable-next-line camelcase
+  // const removeBookFromStore = (item_id) => {
+  //  dispatch(removeBook(item_id));
+  // };
 
   // display books
   return (
     <main>
       <div className="books-container">
-        <BookList removeBook={removeBookFromStore} />
+        <BookList />
         <form className="add-form">
-          <input type="text" placeholder="Title" onChange={(e) => setTitle(e.target.value)} />
-          <input type="text" placeholder="Author" onChange={(e) => setAuthor(e.target.value)} />
+          <input type="text" placeholder="Title" value={title} onChange={(e) => setTitle(e.target.value)} />
+          <input type="text" placeholder="Category" value={category} onChange={(e) => setCategory(e.target.value)} />
           <button type="button" onClick={submitBookToStore}>Add Book</button>
         </form>
       </div>
--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,6 +1,10 @@
+/* eslint-disable camelcase */
+import axios from 'axios';
+
 // Constants:
 const ADD_BOOK = 'bookstore/books/ADD_BOOK';
 const REMOVE_BOOK = 'bookstore/books/REMOVE_BOOK';
+const GET_BOOKS = 'bookstore/books/GET_BOOKS';
 
 // initializes state
 const initialState = [];
@@ -16,13 +20,52 @@ export const removeBook = (payload) => ({
   payload,
 });
 
+export const getBooks = (payload) => ({
+  type: GET_BOOKS,
+  payload,
+});
+
+export const createStoreAPI = () => {
+  axios.post('https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/')
+    .then((response) => {
+      console.log(response.data);
+    });
+};
+
+export const fetchBooks = () => async (dispatch) => {
+  const books = await fetch('https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/bw6bJP6J7bXVfjB5iYY0/books');
+  const data = await books.json();
+  const displayBooks = Object.entries(data).map(([item_id, book]) => {
+    const { category, title } = book[0];
+    return {
+      item_id,
+      category,
+      title,
+    };
+  });
+  dispatch(getBooks(displayBooks));
+};
+
+export const removeBookAPI = (payload) => async (dispatch) => {
+  await axios.delete(`https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/bw6bJP6J7bXVfjB5iYY0/books/${payload}`);
+  dispatch(removeBook(payload));
+};
+
+export const addBookAPI = (payload) => async (dispatch) => {
+  await axios
+    .post('https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/bw6bJP6J7bXVfjB5iYY0/books', payload);
+  dispatch(addBook(payload));
+};
+
 // reducer
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case ADD_BOOK:
       return [...state, action.payload];
+    case GET_BOOKS:
+      return action.payload;
     case REMOVE_BOOK:
-      return state.filter((book) => book.id !== action.payload);
+      return state.filter((book) => book.item_id !== action.payload);
     default:
       return state;
   }
