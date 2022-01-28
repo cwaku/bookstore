76629ed71a07ac39ecf7a0a8f5e28cab493474a8
Ricky Mormor <rickymormor1@gmail.com>
2022-01-28 19:21:38 +0000

    Adds style rules to book items

--- src/components/BookItem.js
+++ src/components/BookItem.js
@@ -1,4 +1,5 @@
 import { useDispatch } from 'react-redux';
+import { CircularProgressbar, buildStyles } from 'react-circular-progressbar';
 import { removeBookAPI } from '../redux/books/books';
 
 const BookItem = (book) => {
@@ -10,23 +11,60 @@ const BookItem = (book) => {
   const removeBookFromStore = () => {
     dispatch(removeBookAPI(item_id));
   };
+  const percentage = Math.floor(Math.random() * 80);
+
   return (
     <div className="bookItem">
       <div className="bookItem__info">
-        <span>{category}</span>
-        <h3>{title}</h3>
-        <span>Author</span>
+        <div className="bookInfo">
+          <span className="category">{category}</span>
+          <h3 className="title">{title}</h3>
+          <span className="author">Ricky Mormor</span>
+        </div>
         <div className="bookItem__actions">
-          <button type="button" className="bookItem_actions_BTN">Comments</button>
-          <button type="button" className="bookItem_actions_BTN" onClick={removeBookFromStore}>Remove</button>
-          <button type="button" className="bookItem_actions_BTN">Edit</button>
+          <button type="button" className="bookItem_actions_BTN">
+            Comments
+          </button>
+          <button
+            type="button"
+            className="bookItem_actions_BTN"
+            onClick={removeBookFromStore}
+          >
+            Remove
+          </button>
+          <button type="button" className="bookItem_actions_BTN">
+            Edit
+          </button>
         </div>
       </div>
-      <div className="Oval-2" />
+      <div className="Oval-2">
+        <CircularProgressbar
+          value={percentage}
+          // text={`${percentage}%`}
+          styles={buildStyles({
+            // Rotation of path and trail, in number of turns (0-1)
+            rotation: 0.25,
+            strokeLinecap: 'butt',
+            textSize: '16px',
+            pathTransitionDuration: 0.5,
+            pathColor: `rgba(2, 144, 255, ${percentage / 80})`,
+            textColor: '#f88',
+            trailColor: '#ebebeb',
+            backgroundColor: '#0290ff',
+          })}
+          className="progress"
+        />
+        <div className="percentage">
+          <p className="perc">{ `${Math.floor(Math.random() * 80)}%`}</p>
+          <p className="completed">Completed</p>
+        </div>
+      </div>
       <div className="Updates-Panel">
         <p className="currentChapter">CURRENT CHAPTER</p>
         <p className="ChapterNumber">Chapter 16</p>
-        <button type="button" className="progressBtn">UPDATE PROGRESS</button>
+        <button type="button" className="progressBtn">
+          UPDATE PROGRESS
+        </button>
       </div>
     </div>
   );
